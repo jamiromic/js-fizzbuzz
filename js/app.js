1f31f10a36fd5575887aa4602d3d56640080d929
jamiromic <jamiromic@gmail.com>
2022-07-22 16:21:56 +0200

    add_comment

--- js/app.js
+++ js/app.js
@@ -5,16 +5,17 @@ for (let i = 0; i < 100; i++ ) {
 //  di iniziare dal numero 1 e finire non 99 ma 100
     let number = i + 1;
 
+    // Se il numero è divisibile per 5 e per 3 scriverò FizzBuzz
     if (number % 5 === 0 & number % 3 === 0) {
         number = 'FizzBuzz';
 
     } 
-
+    // Se il numero è divisibile per 3 scriverò Fizz
     if (number % 3 === 0) {
         number = 'Fizz';
 
     }
-
+    // Se il numero è divisibile per 5 scriverò Buzz
     if (number % 5 === 0) {
         number = 'Buzz';
 
